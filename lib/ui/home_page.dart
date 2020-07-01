df95537b16044db6d0774a6639c35763a1c0c0bd
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-07-01 13:34:23 -0300

    first commit

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+_getGifs() async {
+
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
