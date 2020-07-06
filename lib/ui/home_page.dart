76c3fed1fec5150e2680a882dd7e6e8ddba52636
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-07-06 17:14:21 -0300

    ,

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,15 +7,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _search;
 
-_getGifs() async {
+  int _offset = 0;
 
-}
+  _getGifs() async {
+    http.Response response;
+
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=gNp2RjS2zs032jPNHXQvENTY6BLbFjmu&limit=20&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=gNp2RjS2zs032jPNHXQvENTY6BLbFjmu&q=$_search&limit=20&offset=75&rating=G&lang=en");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
