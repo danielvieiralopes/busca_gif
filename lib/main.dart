984f16373ffa609603d70a38b06baa255b1ba42c
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-07-08 17:03:28 -0300

    savepoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:busca_gif/ui/home_page.dart';
@@ -5,5 +6,6 @@ import 'package:busca_gif/ui/home_page.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,7 +13,7 @@ class _HomePageState extends State<HomePage> {
 
   int _offset = 0;
 
-  _getGifs() async {
+  Future<Map> _getGifs() async {
     http.Response response;
 
     if (_search == null)
@@ -19,11 +21,46 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=gNp2RjS2zs032jPNHXQvENTY6BLbFjmu&limit=20&rating=G");
     else
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=gNp2RjS2zs032jPNHXQvENTY6BLbFjmu&q=$_search&limit=20&offset=75&rating=G&lang=en");
+          "https://api.giphy.com/v1/gifs/search?api_key=gNp2RjS2zs032jPNHXQvENTY6BLbFjmu&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+         Padding(
+           padding: EdgeInsets.all(10.0),
+           child:TextField(
+            decoration: InputDecoration(
+              labelText: "Pesquise Aqui!",
+              labelStyle: TextStyle(color: Colors.white),
+              border: OutlineInputBorder()
+            ),
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+            textAlign: TextAlign.center,
+            
+          ),
+         )
+        ],
+      ),
+    );
   }
 }
